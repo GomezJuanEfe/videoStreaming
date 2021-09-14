afbb028eae3a27a29dde9d2e085a82377110c439
Juan Gomez <jf.gomezramiez@gmail.com>
2021-09-13 21:16:23 -0500

    Proyecto terminado, se agregaron las transiciones del carousel

--- home-index.js
+++ home-index.js
@@ -1,6 +1,5 @@
-/* const userName = prompt("Ingresa tu nombre o un nickname","Nombre");
+const userName = prompt("Ingresa tu nombre o un nickname","Nombre");
 
 const tagName = document.getElementById("UserName");
 
 tagName.innerText = userName;
- */
